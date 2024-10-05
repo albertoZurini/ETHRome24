10ca18b3ac927d5332772e66280825c28820866f
Federico Bertossi <livebymax@gmail.com>
2024-10-05 03:39:44 +0200

    Revert "feat: added Google SignIn"
    This reverts commit 189f599f7e841616e9eb2b33b1527365fc21f077.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
 import NfcReader from "./components/NFCReader";
-import LoginButton from "./components/OAuthButton";
-import { GetServerSideProps } from "next";
-import { getSession } from "next-auth/react";
 
 export default function Home() {
   return (
@@ -14,7 +11,6 @@ export default function Home() {
       <main style={{ padding: '20px', textAlign: 'center' }}>
         <h1>NFC Reader Test Page</h1>
         <NfcReader />
-        <LoginButton />
       </main>
     </div>
   );
