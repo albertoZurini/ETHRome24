ca0d0b05e77e934cda09bac30a6beaec2388b7d4
albertoZurini <alberto.zurini@gmail.com>
2024-10-05 04:23:20 +0200

    feat: reimplement google auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+import NextAuth, { NextAuthOptions, Session } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }): Promise<Session> {
+      if (session.user) {
+        if (typeof token.email === 'string') {
+          session.user.email = token.email; 
+        }
+      }
+      return session;
+    },
+    async jwt({ token, account }): Promise<any> {
+      if (account && 'email' in account) {
+        token.email = account.email as string;
+      }
+      return token;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+"use client"
+
+import { SessionProvider } from 'next-auth/react';
+import GoogleLoginButton from './components/GoogleLoginButton';
+
 import Head from "next/head";
 import NfcReader from "./components/NFCReader";
 
-export default function Home() {
+const Home: React.FC = () => { // Use React.FC to define the component type
   return (
-    <div>
-      <Head>
-        <title>NFC Reader Test</title>
-        <meta name="description" content="Test NFC Reader functionality" />
-      </Head>
-      <main style={{ padding: '20px', textAlign: 'center' }}>
-        <h1>NFC Reader Test Page</h1>
-        <NfcReader />
-      </main>
-    </div>
+    <SessionProvider>
+      <div>
+        <Head>
+          <title>NFC Reader Test</title>
+          <meta name="description" content="Test NFC Reader functionality" />
+        </Head>
+        <main style={{ padding: '20px', textAlign: 'center' }}>
+          <h1>NFC Reader Test Page</h1>
+          <NfcReader />
+          <GoogleLoginButton /> {/* Add your Google Login Button here */}
+        </main>
+      </div>
+    </SessionProvider>
   );
-}
+};
+
+export default Home;
